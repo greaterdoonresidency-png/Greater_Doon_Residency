e0f93776ca1d61a3609c008c349be52b04d5e3c3
greaterdoonresidency-png <greaterdoonresidency@gmail.com>
2026-08-04 02:16:09 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GreaterDoonResidency from "./GreaterDoonResidency.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GreaterDoonResidency />
+  </React.StrictMode>
+);
